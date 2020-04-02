02f805c671b2b34506b50ce2b410ee6104e24082
Mariam Haider <mfh.haider@gmail.com>
2020-04-02 22:46:56 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -3,17 +3,5 @@ class Song
  
   attr_accessor :title, :artist
  
-  def initialize(title)
-    @title = title
-  end
- def artist
-   @artist
- end 
- 
- def artist=(artist)
-  @artist = artist
- end
 end
 
-
-binding.pry
