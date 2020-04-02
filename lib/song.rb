4f054eb34a930963d394436b03d4f4cce940d6c7
Mariam Haider <mfh.haider@gmail.com>
2020-04-02 22:32:57 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,12 +1,18 @@
 require 'pry'
 class Song
  
-  attr_accessor :title
+  attr_accessor :title, :artist
  
   def initialize(title)
     @title = title
   end
+ def artist
+   @artist
+ end 
+ 
+ def artist=(artist)
+  @artist = artist
+ end
 end
-
 
 binding.pry
